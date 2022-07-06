9c86403c2a98da104bd1c4d95e996890e24577b2
Jordi Coll <jjordicoll@gmail.com>
2022-07-06 12:34:05 +0200

    first commit

---
+++ random-quote-generator/script.js
@@ -0,0 +1,76 @@
+let apiQuotes = []
+
+// Document Elements
+const quoteContainer = document.querySelector('.quote-container')
+const quoteText = document.querySelector('.quote-text')
+const quoteTextIn = document.querySelector('#quote')
+const authorTextIn = document.querySelector('#author')
+const loader = document.querySelector('.loader')
+
+// show new quote
+function newQuote() {
+  const n = Math.floor(Math.random() * apiQuotes.length)
+  const author =  apiQuotes[n].author
+  const quote =  apiQuotes[n].text
+  authorTextIn.innerText = author || "unknown" // show unknown when null or ''
+
+  // check lenght of quote
+  if (quote.length > 100) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
+  complete()
+  quoteTextIn.innerText = quote
+}
+
+
+function tweetQuote(quote, author) {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteTextIn.textContent} - ${authorTextIn.textContent}`
+  window.open(twitterUrl, '_blank'); // open twitter on new window
+}
+
+// loading function -- show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.style.display = 'none'
+}
+
+// complete function
+function complete() {
+  loader.hidden = true;
+  quoteContainer.style.display = 'flex'
+}
+
+
+// Get Quoates from API
+async function getQuotes() {
+  loading()
+  const apiUrl = 'https://type.fit/api/quotes'
+  try {
+    const response = await fetch(apiUrl) // only set response when we have data
+    apiQuotes = await response.json()
+    newQuote()
+    console.log(apiQuotes)
+  } catch (error) {
+    // Catch Error Here
+    console.log(error)
+  }
+}
+
+// On load
+getQuotes()
+
+// Button with even delegation
+quoteContainer.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(e.target)
+  if (e.target.classList.contains('fa-arrow-rotate-right') || e.target.classList.contains('refresh-button')) {
+    newQuote()
+  }
+  if (e.target.classList.contains('fa-twitter')) {
+    console.log('ready to open twitter')
+    tweetQuote()  
+
+  }
+})
